ecec7cd49e2c1f2c314285daed14eaef67fee5b4
MariaMelnik <melnikmk@gmail.com>
2020-05-12 23:38:02 +0300

    Improved some widgets design.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,16 +1,17 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/index.html": "bc2e9f65c941bdc1eb9ed7c37eea8de3",
-"/main.dart.js": "0519145279ba3658435adaad5e110806",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/manifest.json": "6809d0720b22bab18ffc15bc1ad43055",
-"/assets/LICENSE": "65be1e2fcb241675bd7d9e83518e6e7e",
-"/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+  "index.html": "bc2e9f65c941bdc1eb9ed7c37eea8de3",
+"/": "bc2e9f65c941bdc1eb9ed7c37eea8de3",
+"main.dart.js": "2f209e3e1531863a7ba533fe53a1a9c4",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "6809d0720b22bab18ffc15bc1ad43055",
+"assets/LICENSE": "e020bf2e3088eb8e1147b7397c917ebd",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
 
 self.addEventListener('activate', function (event) {
@@ -32,9 +33,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
